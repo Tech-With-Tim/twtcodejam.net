06afedadadf4d379b0a245c03afdbe9065d8b5e9
12944qwerty <qwerty12944qwerty@gmail.com>
2020-09-20 14:06:13 -0500

    error to danger

--- TWT/settings.py
+++ TWT/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import our_secrets
+from django.contrib.messages import constants as messages
+
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger' # Make error messages have danger class on bootstrap
+}
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
