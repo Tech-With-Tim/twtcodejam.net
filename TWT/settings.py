d04e59e72a831e081057cc727587829af0a81498
shubhaankar <shubhaankar@hotmail.com>
2020-10-05 12:23:18 +0530

    media root

--- TWT/settings.py
+++ TWT/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'TWT', 'static')
 ]
