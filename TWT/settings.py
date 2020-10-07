4a5fc5bc47d6d8a61ef6eab7a2e3c5878e4b0d03
Correct_skill <shubhaankar@hotmail.com>
2020-10-07 17:24:22 +0530

    Update settings.py

--- TWT/settings.py
+++ TWT/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = our_secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = our_secrets.ALLOWED_HOSTS
 
