0c825bec81b38d8582bf9dd307c1d9b76f6ea2e5
Correct_skill <shubhaankar@hotmail.com>
2021-01-07 10:29:12 +0530

    Delete start-server.sh

--- start-server.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# start-server.sh
-if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (python manage.py createsuperuser --no-input)
-fi
-(gunicorn TWT.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
-nginx -g "daemon off;"
